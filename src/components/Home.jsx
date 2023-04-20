a74d888f54c01b075450514c45304201c4e0dd56
Fonta22 <paufontasp@gmail.com>
2023-04-20 23:25:38 +0200

    Special offers done (en precarietat)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 function Home() {
     const [specials, setSpecials] = useState([]);
@@ -16,7 +16,22 @@ function Home() {
         <h1>Steam&trade; deals of the week</h1>
         <h2>Special offers</h2>
         {specials.map((item, i) => {
-            return <p key={i}>{item.name}</p>
+            return (<>
+                <div class="card" style={{ width: '18rem' }}>
+                    <img src={item.header_image} className="card-img-top" alt="Header Image" href={'https://store.steampowered.com/app/' + item.id} />
+                    <div className="card-body shadow">
+                        <h5 className="card-title">{item.name}</h5>
+                        <span className="card-text"><strike>{(item.original_price / 100).toFixed(2)} €</strike> <span className='badge text-bg-success'>-{item.discount_percent} %</span></span>
+                        <h5 className='mt-1'>{(item.final_price / 100).toFixed(2)} €</h5>
+                        <a href={'https://store.steampowered.com/app/' + item.id} className="btn btn-primary mt-1">View in Steam&trade;</a>
+                    </div>
+                </div>
+                <br />
+                {/*<div>
+                    <img src={item.header_image} alt="Header" key={i} />
+                    <p key={i}>{item.name}</p>
+                </div>*/}
+            </>)
         })}
     </>);
 }
